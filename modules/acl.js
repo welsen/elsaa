dd7c572fbddcfbd392fbaa8628b4bcfbc548c5b5
David Onofer <david.onofer@gmail.com>
2014-02-06 07:19:04 +0100

    Extending ACL

--- modules/acl.js
+++ modules/acl.js
@@ -1,13 +1,16 @@
 var Acl = (function () {
     function Acl(db) {
         this.DB = db;
-        this.UserTable = 'ACL_USERS';
-        this.LevelsTable = 'ACL_LEVELS';
+        this.UsersTable = 'ACL_USERS';
+        this.RolesTable = 'ACL_ROLES';
+        this.PermissionsTable = 'ACL_PERMISSIONS';
+        this.RolePermissionsTable = 'ACL_ROLEPERMISSIONS';
+        this.UserRolesTable = 'ACL_USERROLES';
         this.LastResult = null;
 
         var self = this;
 
-        self.DB.get("SELECT * FROM sqlite_master WHERE type = 'table' AND tbl_name = ?", this.UserTable, function (error, row) {
+        self.DB.get("SELECT * FROM sqlite_master WHERE type = 'table' AND tbl_name = ?", self.UsersTable, function (error, row) {
             if (error === null) {
                 if (row === undefined) {
                     self.DB.run("CREATE TABLE ACL_USERS(\
@@ -24,15 +27,62 @@ var Acl = (function () {
                 }
             }
         });
-        self.DB.get("SELECT * FROM sqlite_master WHERE type = 'table' AND tbl_name = ?", this.LevelsTable, function (error, row) {
+        self.DB.get("SELECT * FROM sqlite_master WHERE type = 'table' AND tbl_name = ?", self.RolesTable, function (error, row) {
             if (error === null) {
                 if (row === undefined) {
-                    self.DB.run("CREATE TABLE ACL_LEVELS(\
+                    self.DB.run("CREATE TABLE ACL_ROLES(\
                             ID          INTEGER     PRIMARY KEY AUTOINCREMENT,\
-                            LEVELNAME   TEXT        NOT NULL,\
+                            ROLENAME    TEXT        NOT NULL,\
+                            DESCRIPTION TEXT        NOT NULL,\
+                            LEFT        INTEGER     NOT NULL,\
+                            RIGHT       INTEGER     NOT NULL,\
                             CREATED     INTEGER     NOT NULL,\
                             MODIFIED    INTEGER     NOT NULL,\
                             ACTIVE      BOOLEAN     NOT NULL\
+                        );");
+                }
+            }
+        });
+        self.DB.get("SELECT * FROM sqlite_master WHERE type = 'table' AND tbl_name = ?", self.PermissionsTable, function (error, row) {
+            if (error === null) {
+                if (row === undefined) {
+                    self.DB.run("CREATE TABLE ACL_PERMISSIONS(\
+                            ID          INTEGER     PRIMARY KEY AUTOINCREMENT,\
+                            PERMISSON   TEXT        NOT NULL,\
+                            DESCRIPTION TEXT        NOT NULL,\
+                            LEFT        INTEGER     NOT NULL,\
+                            RIGHT       INTEGER     NOT NULL,\
+                            CREATED     INTEGER     NOT NULL,\
+                            MODIFIED    INTEGER     NOT NULL,\
+                            ACTIVE      BOOLEAN     NOT NULL\
+                        );");
+                }
+            }
+        });
+        self.DB.get("SELECT * FROM sqlite_master WHERE type = 'table' AND tbl_name = ?", self.RolePermissionsTable, function (error, row) {
+            if (error === null) {
+                if (row === undefined) {
+                    self.DB.run("CREATE TABLE ACL_ROLEPERMISSIONS(\
+                            ROLEID       INTEGER     NOT NULL,\
+                            PERMISSIONID INTEGER     NOT NULL,\
+                            CREATED      INTEGER     NOT NULL,\
+                            MODIFIED     INTEGER     NOT NULL,\
+                            ACTIVE       BOOLEAN     NOT NULL,\
+                            PRIMARY KEY (ROLEID, PERMISSIONID)\
+                        );");
+                }
+            }
+        });
+        self.DB.get("SELECT * FROM sqlite_master WHERE type = 'table' AND tbl_name = ?", self.UserRolesTable, function (error, row) {
+            if (error === null) {
+                if (row === undefined) {
+                    self.DB.run("CREATE TABLE ACL_USERROLES(\
+                            USERID      INTEGER     NOT NULL,\
+                            ROLEID      INTEGER     NOT NULL,\
+                            CREATED     INTEGER     NOT NULL,\
+                            MODIFIED    INTEGER     NOT NULL,\
+                            ACTIVE      BOOLEAN     NOT NULL,\
+                            PRIMARY KEY (USERID, ROLEID)\
                         );");
                 }
             }
